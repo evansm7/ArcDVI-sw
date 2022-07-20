876aa12ea700f4291128be21014aa9b7e00c0409
Matt Evans <matt@ozlabs.org>
2022-07-20 14:35:56 +0100

    Initial import of mostly-working ArcDVI module
    The module provides two extension modes, palette support, and some
    palette-changing *commands.  There are still issues with extended palette
    modes in the Desktop.

---
+++ mod_arcdvi/riscos_defs.h
@@ -0,0 +1,54 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021-2022 Matt Evans
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef RISCOS_DEFS_H
+#define RISCOS_DEFS_H
+
+#define SWI_X           0x20000
+#define SWI_OS_WRITEC   0x0
+#define SWI_OS_WRITES   0x1
+#define SWI_OS_WRITE0   0x2
+#define SWI_OS_NEWLINE  0x3
+#define SWI_OS_FILE     0x08
+#define SWI_OS_GBPB     0x0c
+#define SWI_OS_FIND     0x0d
+#define SWI_OS_MODULE   0x1e
+#define SWI_OS_CLAIM    0x1f
+#define SWI_OS_RELEASE  0x20
+#define SWI_OS_GSTRANS  0x27
+#define SWI_OS_WRITEI   0x100
+#define SWI_COLOURTRANS_WRITEPALETTE    0x4075d
+
+#define V_BIT           (1 << 28)
+
+#define Service_ModeExtension   0x50
+
+#define Vector_PaletteV         0x23
+
+// Embedded string:
+#define ES(str)        swi     SWI_OS_WRITES | SWI_X ; \
+        .asciz str                                   ; \
+        .align
+
+#endif
